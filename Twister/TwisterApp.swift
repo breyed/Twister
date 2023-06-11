448c15ffe1487e2844135b3939a6b03f51951921
Edward Brey <edward@breyfamily.net>
2023-06-11 13:06:48 -0400

    Initial Commit

---
+++ Twister/TwisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwisterApp.swift
+//  Twister
+//
+//  Created by Edward Brey on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TwisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
