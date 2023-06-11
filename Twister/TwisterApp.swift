791739d5b4395ff59390e2649056c3da45400319
Edward Brey <edward@breyfamily.net>
2023-06-11 16:30:12 -0400

    Omit voice when in silent model.

--- Twister/TwisterApp.swift
+++ Twister/TwisterApp.swift
@@ -4,6 +4,10 @@ import SwiftUI
 @main
 struct TwisterApp: App {
     let speechSynthesizer = AVSpeechSynthesizer()
+    
+    init() {
+        try? AVAudioSession.sharedInstance().setCategory(.ambient)
+    }
 
     var body: some Scene {
         WindowGroup {
